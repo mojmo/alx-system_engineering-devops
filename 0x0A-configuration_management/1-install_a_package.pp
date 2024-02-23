389348aae07af9470e8b9136b31149c08d8594ee
mojtaba <mojtaba.mohamed187@gmail.com>
2024-02-23 19:11:05 +0200

    change manifest

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
 # This Puppet manifest ensures that Flask version 2.1.0 is installed using pip3
-package { 'flask':
-    ensure   => '2.1.0',
-    provider => 'pip3'
+
+exec { 'install flask':
+    command => '/usr/bin/pip3 install Flask==2.1.0',
 }
