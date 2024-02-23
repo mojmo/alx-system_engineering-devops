f0ecb3f801b4343046a49d5ad7322d894f04dec3
mojtaba <mojtaba.mohamed187@gmail.com>
2024-02-23 19:43:16 +0200

    ensure werkzeug 2.1.1 installed

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,10 @@
 # This Puppet manifest ensures that Flask version 2.1.0 is installed using pip3
 
-exec { 'install flask':
-    command => '/usr/bin/pip3 install Flask==2.1.0',
+exec {'install-flask':
+require => Exec['werkzeug-installed'],
+command => '/usr/bin/pip3 install flask==2.1.0'
+}
+
+exec {'werkzeug-installed':
+command => '/usr/bin/pip3 install werkzeug==2.1.1'
 }
