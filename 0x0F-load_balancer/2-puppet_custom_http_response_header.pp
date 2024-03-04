ecc34250b775b9047816e9f5e6d05461f849b882
mojtaba <mojtaba.mohamed187@gmail.com>
2024-03-05 00:09:56 +0200

    define relationships between resources using

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -7,19 +7,19 @@ exec { 'update packages':
   provider => 'shell',
   command  => 'sudo apt-get -y update',
 }
-
+->
 package { 'nginx':
     ensure   => 'present',
     provider => apt,
 }
-
+->
 file_line { 'add new HTTP header':
   ensure => 'present',
   path   => '/etc/nginx/sites-available/default',
   after  => 'listen 80 default_server;',
   line   => "add_header X-Served-By \"${hostname}\";"
 }
-
+->
 exec {'restart_server':
     provider => shell,
     command  => 'sudo service nginx restart',
